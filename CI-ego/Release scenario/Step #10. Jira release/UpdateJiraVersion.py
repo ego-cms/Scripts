71b221dc778a5a4b92324a0ebe3969179876a9f6
Igor <>
2019-03-21 17:19:16 +0300

    Added scripts that work with Jira

---
+++ CI-ego/Release scenario/Step #10. Jira release/UpdateJiraVersion.py
@@ -0,0 +1,77 @@
+# Input
+# %env.ProjectId% : Jira project id  																	|| 1000
+# %env.AppVersion% : new app version
+# %env.JiraUrl% : url path for jira 																	|| https://test.atlassian.net
+# %env.JiraBotMailAddress% : email address for user which will interact with jira  						|| test@ego-cms.com
+# %env.JiraBotApiToken% : api token for user which will interact with jira 								|| Abababagalamaga
+
+import requests
+import sys
+
+
+def create_version_and_get_id():
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    }
+
+    data = '{ "name":"%env.AppVersion%", "projectId":"%env.ProjectId%" }'
+    # send request for adding new version
+    response = requests.post('%env.JiraUrl%/rest/api/3/version', headers=headers, data=data,
+                             auth=('%env.JiraBotMailAddress%', '%env.JiraBotApiToken%'))
+
+print(response.json())
+
+# return new version id
+    return response.json()["id"]
+
+
+def do_release(current_version, new_version):
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    }
+    
+    data = '{"released":"true", "moveUnfixedIssuesTo":"'+new_version+'"}'
+    # send request for changing version status to release
+    response = requests.put('%env.JiraUrl%/rest/api/3/version/'+current_version, headers=headers, data=data,
+                            auth=('%env.JiraBotMailAddress%', '%env.JiraBotApiToken%'))
+
+print(response)
+
+
+def get_current_version_id():
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    }
+
+    # send request for getting all project's versions
+    response = requests.get('%env.JiraUrl%/rest/api/3/project/%env.ProjectKey%/versions', headers=headers,
+                            auth=('%env.JiraBotMailAddress%', '%env.JiraBotApiToken%'))
+
+# first version that's not archived and not released is returned
+for version in response.json():
+    if not version["released"] and not version["archived"]:
+        print(version)
+        return version["id"]
+    
+    print("Current version not found")
+    sys.exit()
+
+
+if "%env.SkipRelease%" == "true":
+    sys.exit()
+
+current_version_id = get_current_version_id()
+
+print("current version - " + current_version_id)
+
+sys.stdout.write("##teamcity[setParameter name='env.JiraPreviousVersionId' value='"+current_version_id+"']")
+sys.stdout.flush()
+
+new_version_id = create_version_and_get_id()
+
+print("new version - " + new_version_id)
+
+do_release(current_version_id, new_version_id)
---
+++ CI-ego/Release scenario/Step #11. Update confluence page/UpdateConfluencePage.py
@@ -0,0 +1,69 @@
+# Input:
+# %env.JiraUrl% : url path for jira			                                                        || https://test.atlassian.net
+# %env.JiraBotMailAddress% : email address for user which will interact with jira                   || test@ego-cms.com
+# %env.JiraBotApiToken% : api token for user which will interact with jira 							|| Abababagalamaga
+
+import json
+import requests
+import sys
+from bs4 import BeautifulSoup
+
+
+def get_page_json():
+    # get content of confluence
+    response = requests.get('%env.JiraUrl%/wiki/rest/api/content/%env.ConfluencePageNumber%?expand=body.storage,version',
+                            auth=('%env.JiraBotMailAddress%', '%env.JiraBotApiToken%'))
+
+    return response.json()
+
+
+def get_page_html(json):
+    # create BeautifulSoup from page content
+    return BeautifulSoup(json["body"]["storage"]["value"], 'html.parser')
+
+
+def get_new_page_version(json):
+    return json["version"]["number"] + 1;
+
+
+def insert_new_tr_tag(in_soup):
+    # create new BeautifulSoup object with tr content
+    newTr = BeautifulSoup(
+        '<tr><td><a href=\"%env.JiraUrl%/browse/%env.ProjectKey%/fixforversion/%env.JiraPreviousVersionId%\">%env.AppPreviousVersion%</a></td><td><a href=\"%env.BuildServerUrl%/repository/download/%system.teamcity.buildType.id%/%teamcity.build.id%:id/%env.ReleaseApkFinalName%\">Build</a></td></tr>')
+
+    # get current number of tr
+    line_numbers = len(in_soup.table.find_all('tr'))
+
+    # get last tr tag, and new tr tag after
+    in_soup.table.find_all('tr')[line_numbers - 1].insert_after(newTr.tr)
+
+
+def send_update_page(in_soup, new_version, page_title):
+    # request headers
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
+
+    # request body
+    data = json.dumps({"id": %env.ConfluencePageNumber%, "type": "page", "title": page_title,
+                       "body": {"storage": {"value": str(in_soup), "representation": "storage"}},
+                       "version": {"number": new_version}})
+
+    # send request to modify page
+    response = requests.put('%env.JiraUrl%/wiki/rest/api/content/%env.ConfluencePageNumber%', headers=headers, data=data,
+                            auth=('%env.JiraBotMailAddress%', '%env.JiraBotApiToken%'))
+
+
+if "%env.SkipRelease%" == "true":
+    sys.exit()
+
+page_json = get_page_json()
+
+html = get_page_html(page_json)
+
+version = get_new_page_version(page_json)
+
+insert_new_tr_tag(html)
+
+send_update_page(html, version, page_json["title"])
